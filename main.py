571c8489809cc567f044f8835d073f0beef86e2d
James Vance <jvance@ictp.it>
2017-02-06 19:03:34 +0100

    Added function dictionary and filling of lists

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import sys
+import numpy as np
+
+f_list = { 1: lambda x: x }
 
 if __name__ == '__main__':
 	try:
-		print(int(sys.argv[1]))
-	except (IndexError):
+		f = f_list[int(sys.argv[1])]
+	except (IndexError,ValueError,KeyError):
 		print("Please provide integer input")
 		raise
-		
+	
+	xval = np.arange(-5.0,+5.1,0.1)
+	yval = f(xval)
+
+	print(xval)
+	print(yval)
